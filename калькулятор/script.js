6deb0c4988e56f9e8d999541f9df148976cca65a
lioshkaa <89130863+lioshkaa@users.noreply.github.com>
2021-11-24 09:27:51 +0300

    Add files via upload

---
+++ калькулятор/script.js
@@ -0,0 +1,19 @@
+function insert(num){
+  document.form.textView.value=document.form.textView.value+num;
+
+}
+function clean(){
+  document.form.textView.value="";
+}
+function back(){
+  let exp=document.form.textView.value;
+  document.form.textView.value=exp.substring(0,exp.length-1)
+}
+function equal(){
+  let exp=document.form.textView.value;
+  if(exp){
+    document.form.textView.value=eval(exp);
+
+  }
+
+}
