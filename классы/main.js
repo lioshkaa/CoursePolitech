63d00df45a12800c52f64fadeb0672601b5a17f0
lioshkaa <89130863+lioshkaa@users.noreply.github.com>
2021-11-27 00:22:54 +0300

    Add files via upload

---
+++ классы/main.js
@@ -0,0 +1,30 @@
+class Component{
+    constructor(selector){
+        this.$el=document.querySelector(selector);
+    }
+    hide(){
+        this.$el.style.display='none';
+    }
+    show(){
+        this.$el.style.display='block';
+
+    }
+}
+class Box extends Component{
+    constructor(options){
+        super(options.selector)
+        this.$el.style.width=this.$el.style.height=options.size +'px';
+        this.$el.style.background=options.color;
+    }
+}
+const box1=new Box({
+    selector:'#box1',
+    size:100,
+    color:'red'
+})
+
+
+
+
+
+
