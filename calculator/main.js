a83c43a6622ab43b4456f5b125557c22b45ccd83
Lesha <89130863+lioshkaa@users.noreply.github.com>
2021-12-17 21:02:58 +0300

    Add files via upload

---
+++ calculator/main.js
@@ -0,0 +1,58 @@
+const incomeSalary=document.getElementById("income-salary"),
+      incomeFreelance=document.getElementById("income-freelance"),
+      incomeExtra1=document.getElementById("income-extra-1"),
+      incomeExtra2=document.getElementById("income-extra-2");
+
+
+const costsFlat=document.getElementById("costs-flat"),
+      costsHouseServices=document.getElementById("costs-house-services"),
+      costsTransport=document.getElementById("costs-transport"),
+      costsCredit=document.getElementById("costs-credit");
+
+const totalMothInput=document.getElementById("total-month"),
+      totalDayInput=document.getElementById("total-day"),
+      totalYearInput=document.getElementById("total-year");
+
+let totalMonth,totalDay,totalYear;
+
+const moneyBoxRange=document.getElementById("money-box-range"),
+      accumulationInput=document.getElementById("accumulation"),
+      spend= document.getElementById("spend");
+
+let accumulation=0;
+let totalPrecents=0;
+
+const inputs=document.querySelectorAll(".input");
+for(input of inputs){
+    input.addEventListener("input",()=>{
+       countingAvailableMoney();
+       calculationPrecents();
+    })
+}
+
+const strToNum=str=>str.value ? parseInt(str.value):0;
+const countingAvailableMoney=()=>{
+    const totalPerMonth=strToNum(incomeSalary)+strToNum(incomeFreelance)+strToNum(incomeExtra1)+strToNum(incomeExtra2);
+    const totalPerCosts=strToNum(costsFlat)+strToNum(costsHouseServices)+strToNum(costsTransport)+strToNum(costsCredit);
+    totalMonth=totalPerMonth-totalPerCosts;
+    totalMothInput.value=totalMonth;
+}
+
+moneyBoxRange.addEventListener("input",e=>{
+    const totalPrecentEl=document.getElementById("total-precents");
+    totalPrecents=e.target.value;
+    totalPrecentEl.innerHTML=totalPrecents;
+    calculationPrecents();
+});
+
+const calculationPrecents=()=>{
+    accumulation=((totalMonth*totalPrecents)/100).toFixed();
+    accumulationInput.value=accumulation;
+    spend.value=totalMonth-accumulation;
+
+    totalDay=(spend.value/30).toFixed();
+    totalDayInput.value=totalDay;
+
+    totalYear=accumulation*12;
+    totalYearInput.value=totalYear;
+}
